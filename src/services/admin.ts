0240b4343a20cd054bb8d35569c5402074d3a5a4
Uday <rahul.uday07@gmail.com>
2025-05-06 16:18:22 +0530

    admin dashboard done

---
+++ src/services/admin.ts
@@ -0,0 +1,116 @@
+import apiClient from "./api/interceptors";
+
+export interface AdminDashboardUserActivityPayload {
+  user_id: string;
+}
+
+export interface AdminDashboardUserStatsPayload {
+  user_id: string;
+}
+
+export interface AdminDashboardUserActivityResponse {
+  id: string;
+  name: string;
+  email: string;
+  role: string;
+  division: string;
+  department: string;
+  addedOn: string;
+  status: string;
+  assignedSimulations: number;
+  completionRate: number;
+  adherenceRate: number;
+  averageScore: number;
+  activatedOn: string;
+  deActivatedOn: string;
+  loginCount: number;
+  lastLoginOn: string;
+  lastSessionDuration: number;
+}
+
+export interface AdminDashboardUserStatsResponse {
+  new_users: {
+    total_users: number;
+    breakdown: {
+      admin: number;
+      manager: number;
+      designer: number;
+      trainees: number;
+    };
+  };
+  active_users: {
+    total_users: number;
+    breakdown: {
+      admin: number;
+      manager: number;
+      designer: number;
+      trainees: number;
+    };
+  };
+  deactivated_users: {
+    total_users: number;
+    breakdown: {
+      admin: number;
+      manager: number;
+      designer: number;
+      trainees: number;
+    };
+  };
+  daily_active_users: {
+    total_users: number;
+    breakdown: {
+      admin: number;
+      manager: number;
+      designer: number;
+      trainees: number;
+    };
+  };
+  weekly_active_users: {
+    total_users: number;
+    breakdown: {
+      admin: number;
+      manager: number;
+      designer: number;
+      trainees: number;
+    };
+  };
+  monthly_active_users: {
+    total_users: number;
+    breakdown: {
+      admin: number;
+      manager: number;
+      designer: number;
+      trainees: number;
+    };
+  };
+}
+
+export const fetchAdminDashboardUserActivity = async (
+  payload: AdminDashboardUserActivityPayload
+): Promise<AdminDashboardUserActivityResponse[]> => {
+  try {
+    const response = await apiClient.post(
+      "/admin-dashboard/users/activity",
+      payload
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching admin dashboard stats:", error);
+    throw error;
+  }
+};
+
+export const fetchAdminDashboardStats = async (
+  payload: AdminDashboardUserStatsPayload
+): Promise<AdminDashboardUserStatsResponse> => {
+  try {
+    const response = await apiClient.post(
+      "/admin-dashboard/users/stats",
+      payload
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching admin dashboard stats:", error);
+    throw error;
+  }
+};
